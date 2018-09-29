c77d03c586f0b44305947af2f2e45cf964d377a4
gilberto moraes <>
2018-09-28 22:46:56 -0300

    first commit

---
+++ readme.md
@@ -0,0 +1,109 @@
+# ApiRestfulTrait
+
+A simple Laravel trait with restful crud implementation.
+
+## Installation
+
+### With Composer
+
+```
+$ composer require moraesgil/api-restful-trait
+```
+
+```json
+{
+   "require": {
+      "moraesgil/api-restful-trait": "~1.0"
+   }
+}
+```
+### Usage
+
+
+create your routes in routes/web.php or your's routes file
+```php
+Route::resource('/samples', 'YourLaravelController', ['except' => ['create', 'edit', 'show']]);
+
+```
+
+make sure you have a model at App\Entities\YourModel same name of controller to Autoload model entity, or you can inject dependencies, see bellow the following instructions in Extras area...
+```terminal
+//terminal
+php artisan make model YourLaravel
+```
+
+In your Controller
+```php
+
+use Traits\Controllers\ApiRestfulTrait;  //<<<<< add this line
+
+class YourLaravelController extends Controller {
+     use ApiRestfulTrait;  //<<<<< add this line  
+
+     // ..others non-crud methods
+}
+// done just it !!!
+
+```
+
+## Extras
+
+### Getting an index page
+
+May  you have to call a page in index() if not ajax request, we have getPage(), can you override method or just to pass data by constructor, also you can redirect to another page passing the route name;
+
+```php
+//in or controller you can pass
+class YourLaravelController extends Controller {
+   public function __construct()
+   {  
+      //Passing view and data
+       $this->indexView = "youCrudBlade"; //default "crud"
+       $this->indexData = [
+          "pageTitle" => "Page title for exemple"
+       ];
+
+       // OR redirect to named route
+       $this->indexRedirectRouteName ="myroute.indexpage"
+   }
+}
+```
+
+### Autoload model entity
+
+By default it try access a model with same name of controller, you can to pass your entity by name or object in constructor
+
+```php
+class YourLaravelController extends Controller {
+   public function __construct(Mymodel $m)
+   {
+       $this->Model = $m;
+       // or
+       $this->modelName = "Mymodel";
+   }
+}
+```
+
+### Getting data from Entity custom method
+
+By default it return a Laravel paginate getting limit by request, can you override method or call a customMethod from your entitiy passing $modelGetMethod
+
+```php
+class YourLaravelController extends Controller {
+   public function __construct()
+   {
+       $this->modelGetMethodName = "myCustomPaginate";
+   }
+}
+```
+
+## Dependencies
+
+* **EntityValidatorTrait** - *My trait to allow model do self validation* - [EntityValidatorTrait](https://github.com/MoraesGil/EntityValidatorTrait)
+
+## Authors
+
+* **Gilberto Prudêncio Vaz de Moraes** - *Initial work* - [MoraesGil](https://github.com/Moraesgil)
+
+## License
+This project is licensed under the MIT License - see the [LICENSE](LICENSE) file for details
---
+++ src/ApiRestfulTrait.php
@@ -0,0 +1,134 @@
+<?php
+/*
+* @author Gilberto Prudêncio Vaz de Moraes <moraesdev@gmail.com>
+* @copyright Copyright (c) 2017
+* @category PHP Trait
+* @version [1.0]
+* @date     2017-10-10
+*/
+namespace Traits\Controllers;
+
+use Illuminate\Http\Request;
+
+trait ApiRestfulTrait
+{
+  // define a model object
+  protected $Model                  = null;
+  // define a model name
+  protected $modelName              = "";
+  // define a model name
+  protected $modelNamespace         = "Entities";
+  // define data to passing blade view
+  protected $indexData              = [];
+  //define the blade view to call
+  protected $indexView              = "crud";
+  //define a redirect route name to call
+  protected $indexRedirectRouteName = "";
+  //define a getMethodName for getData
+  protected $modelGetMethodName     = "";
+
+  /**
+  * Returns a instance of Model with same Controller name, from Entities folder
+  * @return [Model] [Entity Instance]
+  */
+  protected function Model(){
+    if ($this->Model == null) {
+      $classPath = str_replace_last("Http\Controllers",$this->modelNamespace,get_class());
+      $class = $this->modelName ? str_replace_last((new \ReflectionClass($this))->getShortName(),$this->modelName,$classPath) : str_replace_last("Controller","",$classPath); //$modelName;
+      $this->Model = new $class();
+    }
+    return $this->Model;
+  }
+
+  /**
+  * GM - Display a listing of the resource.
+  *
+  * @return \Illuminate\Http\Response
+  */
+  public function index(Request $request)
+  {
+    if ($request->expectsJson() || ($request->get("json") != null && env('APP_DEBUG', false))) {
+      return $this->getData($request);
+    }
+    return $this->indexRedirectRouteName ? redirect()->route($this->indexRedirectRouteName) : $this->getPage();
+  }
+
+  /*
+  * getPage return a blade view
+  * @param  Request $request
+  * @return [BladeView]
+  */
+  private function getPage()
+  {
+    return view($this->indexView, $this->indexData);
+  }
+
+  /*
+  * [getData description]
+  * @param  Request $request  [description]
+  * @param  integer $paginate [description]
+  * @return [type]            [description]
+  */
+  private function getData(Request $request)
+  {
+    if ($this->modelGetMethodName) {
+      return response()->json($this->Model()->{$this->modelGetMethodName}($request->all()),200 );
+    }
+    return response()->json($this->Model()->paginate($request->get("limit")),200 );
+  }
+
+  /**
+  * GM - Store a newly created resource in storage.
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @return \Illuminate\Http\Response
+  */
+  public function store(Request $request)
+  {
+    if ($this->Model()->hasRules()) {
+      if (!$this->Model()->validate($request->all())) {
+        return response()->json($this->Model()->errors, 400);
+      }
+    }
+    return response()->json($this->Model()->create($request->all()), 201);
+  }
+
+  /**
+  * GM - Display the specified resource.
+  *
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function show($id)
+  {
+    return $this->Model()->findOrFail($id);
+  }
+
+  /**
+  * GM - Update the specified resource in storage.
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function update(Request $request, $id)
+  {
+    if ($this->Model()->hasRules()) {
+      if (!$this->Model()->validate($request->all(), $id)) {
+        return response()->json($this->Model()->errors, 400);
+      }
+    }
+    return response()->json($this->Model()->findOrFail($id)->update($request->all()), 200);
+  }
+
+  /**
+  * GM - Remove the specified resource from storage.
+  *
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function destroy(Request $request, $id)
+  {
+    return response()->json($this->Model()->findOrFail($id)->delete(), 204);
+  }
+}
